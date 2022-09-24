008a32e6582897238291bd6063c5b0c34a80ec78
Ben <toussaint.benoit@gmail.com>
2022-09-24 14:48:20 +0000

    first wf

---
+++ src/wineclassification/wineClassifierTrainFlow.py
@@ -0,0 +1,22 @@
+from metaflow import FlowSpec, step
+
+class WineClassifierTrainFlow(FlowSpec):
+
+    @step
+    def start(self):
+        from sklearn.datasets import load_wine
+        from sklearn.model_selection import train_test_split
+
+        X, y = load_wine(return_X_y=True)
+        self.train_data, self.test_data, self.train_labels, self.test_labels = train_test_split(X, y, test_size=0.2, random_state=0)
+        print('Data loaded successfully')
+
+        self.next(self.end)
+
+    @step
+    def end(self):
+        self.model = "modelBurger"
+        print('Done')
+
+if __name__ == '__main__':
+    WineClassifierTrainFlow()
